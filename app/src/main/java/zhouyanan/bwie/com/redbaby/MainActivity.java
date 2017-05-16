c1eff2d2a02a83c3e65c82ee112cd8d7b42220ed
MuXIaoHong <932899053@qq.com>
2017-05-16 10:04:49 +0800

    框架初

--- app/src/main/java/zhouyanan/bwie/com/redbaby/MainActivity.java
+++ app/src/main/java/zhouyanan/bwie/com/redbaby/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
@@ -13,7 +12,6 @@ import android.widget.RadioGroup;
 public class MainActivity extends AppCompatActivity {
 
     private RadioGroup radiogroup;
-    private ViewPager viewpager;
     private int[] buttonid;
     private Fragment currentf;
     private HomeFragmentActivity homeFragment;
